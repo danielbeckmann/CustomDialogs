bdd9cb8eb93451b1939c91f5e91f42b3a26f8857
Daniel <danielbeckmann@live.de>
2014-07-13 14:01:38 +0200

    Initial commit

---
+++ CustomDialogs/InputDialog.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Phone.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Dialogs
+{
+    public sealed partial class InputDialog : UserControl
+    {
+        // Popup control for displaying the dialog
+        private Popup popup;
+
+        // For async open/close operation
+        private TaskCompletionSource<string> taskCompletionSource;
+
+        public InputDialog(string content, string title)
+        {
+            this.InitializeComponent();
+            
+            this.TitleTextBlock.Text = title;
+            this.ContentTextBlock.Text = content;
+        }
+
+        /// <summary>
+        /// Opens the dialog.
+        /// </summary>
+        public Task<string> ShowAsync()
+        {
+            this.taskCompletionSource = new TaskCompletionSource<string>();
+            this.CreateDialog();
+
+            return this.taskCompletionSource.Task;
+        }
+
+        // Create a popup control and set content
+        private void CreateDialog()
+        {
+            this.Height = Window.Current.Bounds.Height;
+            this.Width = Window.Current.Bounds.Width;
+
+            this.popup = new Popup();
+            this.popup.Child = this;
+            
+            HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+
+            this.popup.IsOpen = true;
+        }
+
+        // Event handler for back button
+        private void HardwareButtons_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            HardwareButtons.BackPressed -= HardwareButtons_BackPressed;
+            if (this.popup.IsOpen)
+            {
+                this.Close(false);
+                e.Handled = true;
+            }
+        }
+
+        // Closes the dialog
+        private void Close(bool success)
+        {
+            HardwareButtons.BackPressed -= HardwareButtons_BackPressed;
+            this.popup.IsOpen = false;
+
+            if (this.taskCompletionSource != null)
+            {
+                if (success)
+                {
+                    this.taskCompletionSource.SetResult(this.InputTextBox.Text);
+                }
+                else
+                {
+                    this.taskCompletionSource.SetResult(null);
+                }
+            }
+        }
+
+        private void OKButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Close(true);
+        }
+
+        private void CancelButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Close(false);
+        }
+    }
+}
---
+++ CustomDialogs/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=391641 dokumentiert.
+
+namespace CustomDialogs
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet werden kann oder auf die innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new MessageDialog("Hier steht die Nachricht", "Titel");
+            var result = await dialog.ShowAsync();
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var dialog = new InfoDialog("Hier steht die Nachricht", "Titel");
+            var result = await dialog.ShowAsync();
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var dialog = new InputDialog("Bitte geben Sie etwas ein", "Titel");
+            var result = await dialog.ShowAsync();
+
+            if (!string.IsNullOrEmpty(result))
+            {
+                Debug.WriteLine(result);
+            }
+        }
+    }
+}
